048acbe3ca689550b4c30dc05764b2e80de8087b
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-05-13 17:39:10 +0300

    html demo

--- 02_object_oriented_programming/src/index.ts
+++ 02_object_oriented_programming/src/index.ts
@@ -22,28 +22,53 @@ interface IUser {
     };
 };
 
-class User {
-    private _myProp!: string;
+// class User {
+//     private _myProp!: string;
 
-    constructor(public id: number, public username: string) {
-        // this._myProp = '';
-    }
+//     constructor(public id: number, public username: string) {
+//         // this._myProp = '';
+//     }
 
-    set myProp(value: string) {
-        this._myProp = value;
-    }
+//     set myProp(value: string) {
+//         this._myProp = value;
+//     }
 
-    get someProp() {
-        return this._myProp;
-    }
+//     get someProp() {
+//         return this._myProp;
+//     }
+// }
+
+// const u = new User(1, 'aa');
+// u.someProp;
+
+const usernameInput = document.getElementById('username') as HTMLInputElement;
+const addButton = document.getElementById('add-btn') as HTMLButtonElement;
+const userList = document.getElementById('user-list') as HTMLUListElement;
+
+const createLiElement = (value: string) => {
+    const li = document.createElement('li') as HTMLLIElement;
+    li.innerHTML = value;
+    return li;
 }
 
-const u = new User(1, 'aa');
-u.someProp;
+const createAndAppendLi = (value: string) => {
+    const li = createLiElement(value)
+    userList.appendChild(li);
+
+}
+
+addButton.addEventListener('click', () => {
+    createAndAppendLi(usernameInput.value);
+    usernameInput.value = '';
+});
 
 fetch('https://jsonplaceholder.typicode.com/users')
-    .then((res: Response) => res.json<User[]>())
+    .then((res: Response) => res.json<IUser[]>())
     .then((users) => {
         // users.forEach(user => console.log(user.address.street))
-        users.forEach(user => console.log(user.username))
+        users.forEach(user => createAndAppendLi(user.username))
     });
+
+abstract class Test {
+    abstract test(): void;
+}
